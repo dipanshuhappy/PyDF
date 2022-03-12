cbfa7745db9089496e56f777d5689fc5f2e23a28
dipanshuhappy <dipanshuhappy@gmail.com>
2022-03-12 14:36:46 +0530

    proper initalizing of project

---
+++ App.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+from utils import WindowManager
+class App(tk.Tk):
+    def __init__(self, screenName: str = None , baseName: str = None , className: str =None, useTk: bool =None , sync: bool =None , use: str = None ) -> None:
+        if (screenName!=None): super().__init__(screenName, baseName, className, useTk, sync, use)
+        else: super().__init__()
+        WindowManager(self)
+App().mainloop()
---
+++ utils.py
@@ -0,0 +1,28 @@
+import tkinter
+class WindowManager:
+    def __init__(self,app:tkinter.Tk,config:dict={}) -> None:
+        self.config=config
+        self.__do_window_config(app)
+    def __do_window_config(self,app:tkinter.Tk)->None:
+        app.config(self.config)
+        app.rowconfigure(0,weight=1)
+        app.columnconfigure(0,weight=1)
+class Page(tkinter.Frame):
+    def __init__(self,app) -> None:
+        super().__init__(app)
+    def show_page(self)->None:
+        pass
+class FrameManager:
+    def __init__(self,frames:dict[str,Page]) -> None:
+        self.frames=frames
+        self.stack=[]
+    def show_frame(self,id):
+        self.frames[id].show_page()
+        self.stack.append(id)
+    def remove_frame(self,id):
+        self.frames[id].destroy()
+        self.stack.remove(id)
+    def go_back(self):
+        self.frames[id].destroy()
+        self.stack.pop()
+        self.frames[self.stack[-1]].show_page()
