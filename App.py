fa0dfd27af83a0815be5bfdbb9cbde9a79067634
dipanshuhappy <dipanshuhappy@gmail.com>
2022-03-18 21:22:40 +0530

    round button

--- App.py
+++ App.py
@@ -1,17 +1,18 @@
 import sys
 import tkinter as tk
 from components.BackButton import BackButton
+from components.RoundButton import RoundButton
 from pages.MainPage import MainPage
 from pages.PasswordPage import PasswordPage
 from pages.WelcomePage import WelcomePage
 from utils import FrameManager, WindowManager
-
 class App(tk.Tk):
     def __init__(self, screenName: str = None , baseName: str = None , className: str =None, useTk: bool =None , sync: bool =None , use: str = None ) -> None:
         if (screenName!=None): super().__init__(screenName, baseName, className, useTk, sync, use)
         else: super().__init__()
         WindowManager(self)
         BackButton(self)
+        RoundButton(self,200,30,0,0,'hi',lambda :print('kjld'))
         self.initialize_frames()
     def initialize_frames(self):
         self.frame_manager= FrameManager({
@@ -19,7 +20,7 @@ class App(tk.Tk):
             'MainPage':MainPage,
             "PasswordPage":PasswordPage
         },self)
-        self.frame_manager.show_frame('WelcomePage')
+        # self.frame_manager.show_frame('WelcomePage')
 app=App()
 print("App size",sys.getsizeof(app))
 app.mainloop()
---
+++ components/RoundButton.py
@@ -0,0 +1,53 @@
+import imp
+from time import sleep
+from tkinter import Canvas, ttk
+import typing
+from typing import Callable
+import tkinter
+
+from click import command
+from pages.Page import Page
+class RoundButton(ttk.Frame):
+    def __init__(self,parent_frame:tkinter.Tk|tkinter.Frame|Page,width:float|int,height:float|int,x:float|int,y:float|int,text:str,command:Callable,bg_color:str="black",text_color:str="white",**kwargs) -> None:
+        super().__init__(parent_frame)
+        self.bg_color=bg_color
+        self.command=command
+        self.canvas=tkinter.Canvas(self,width=width,height=height)
+        self.border=self.round_rectangle(0,0,width,height,fill=bg_color)
+        self.button_text= self.canvas.create_text(width/2, height/2, text=text,fill=text_color)
+        self.canvas.tag_bind(self.border, "<Button-1>", self.clicked)
+        self.canvas.tag_bind(self.button_text, "<Button-1>",self.clicked)
+        self.show_button()
+    def show_button(self):
+        self.canvas.pack()
+        self.pack()
+    def round_rectangle(self,x1, y1, x2, y2, radius=25, **kwargs):
+        
+        points = [
+                x1+radius, y1,
+                x1+radius, y1,
+                x2-radius, y1,
+                x2-radius, y1,
+                x2, y1,
+                x2, y1+radius,
+                x2, y1+radius,
+                x2, y2-radius,
+                x2, y2-radius,
+                x2, y2,
+                x2-radius, y2,
+                x2-radius, y2,
+                x1+radius, y2,
+                x1+radius, y2,
+                x1, y2,
+                x1, y2-radius,
+                x1, y2-radius,
+                x1, y1+radius,
+                x1, y1+radius,
+                x1, y1
+                ]
+
+        return self.canvas.create_polygon(points, **kwargs, smooth=True)
+    def clicked(self,event):
+        self.canvas.itemconfig(self.border,fill='grey')
+        self.command()
+        self.after(200,lambda: self.canvas.itemconfig(self.border,fill=self.bg_color))
